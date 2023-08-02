80aa166c2c9b0b1e977b90aee0be72f0d9f49682
samerulesapply22 <curryballato@gmail.com>
2023-08-02 17:46:32 +0300

    Add game logic

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -22,9 +22,37 @@ export default function Cards() {
   const [cards, setCards] = useState(cardsArray);
   const [prev, setPrev] = useState(-1);
 
+  function matched(index) {
+    if (cardsArray[index].id === cardsArray[prev].id) {
+      console.log(cardsArray[prev].id);
+      cardsArray[prev].status = "correct";
+      cardsArray[index].status = "correct";
+      setCards([...cards]);
+      setTimeout(() => {
+        cardsArray[prev].status = "out";
+        cardsArray[index].status = "out";
+        setCards([...cards]);
+      }, 2000);
+      
+    } else {
+      cardsArray[prev].status = "incorrect";
+      cardsArray[index].status = "incorrect";
+      setCards([...cards]);
+      setTimeout(() => {
+        cardsArray[prev].status = "back";
+        cardsArray[index].status = "back";
+        setCards([...cards]);
+      }, 1500);
+    }
+    setPrev(-1);
+  }
+
   function handleClick(index) {
     cardsArray[index].status = "active";
     setCards([...cards]);
+    if (prev === -1) setPrev(index);
+    else matched(index);
+    console.log(prev);
   }
 
   const cardsArrayDisplay = cardsArray.map((card, index) => {
