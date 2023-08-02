c670d46ab25107d1d088fcfc3f5a919dba991366
samerulesapply22 <curryballato@gmail.com>
2023-08-02 15:30:56 +0300

    Add functionality for flipping cards

--- src/components/card.jsx
+++
@@ -1,7 +0,0 @@
-const Card = ({card}) => {
-  return <div className="card" >
-     <img src={card.img} alt={card.id} />
-   </div>;
-};
-
-export default Card;
--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,36 +1,43 @@
 import { useState } from "react";
-//import Card from "./card";
-
 const cardsArray = [
-  { id: 1, img: "src/components/assets/happyFace.png", stat: "" },
-  { id: 1, img: "src/components/assets/happyFace.png", stat: "" },
-  { id: 2, img: "src/components/assets/horse.png", stat: "" },
-  { id: 2, img: "src/components/assets/horse.png", stat: "" },
-  { id: 3, img: "src/components/assets/redStar.png", stat: "" },
-  { id: 3, img: "src/components/assets/redStar.png", stat: "" },
-  { id: 4, img: "src/components/assets/treeStars.png", stat: "" },
-  { id: 4, img: "src/components/assets/treeStars.png", stat: "" },
-  { id: 5, img: "src/components/assets/triangle.png", stat: "" },
-  { id: 5, img: "src/components/assets/triangle.png", stat: "" },
-  { id: 6, img: "src/components/assets/triangleRectangle.png", stat: "" },
-  { id: 6, img: "src/components/assets/triangleRectangle.png", stat: "" },
-  { id: 7, img: "src/components/assets/twoStars.png", stat: "" },
-  { id: 7, img: "src/components/assets/twoStars.png", stat: "" },
-  { id: 8, img: "src/components/assets/yellowStar.png", stat: "" },
-  { id: 8, img: "src/components/assets/yellowStar.png", stat: "" },
-];
+  { id: 1, img: "src/components/assets/happyFace.png", status: "back" },
+  { id: 1, img: "src/components/assets/happyFace.png", status: "back" },
+  { id: 2, img: "src/components/assets/horse.png", status: "back" },
+  { id: 2, img: "src/components/assets/horse.png", status: "back" },
+  { id: 3, img: "src/components/assets/redStar.png", status: "back" },
+  { id: 3, img: "src/components/assets/redStar.png", status: "back" },
+  { id: 4, img: "src/components/assets/treeStars.png", status: "back" },
+  { id: 4, img: "src/components/assets/treeStars.png", status: "back" },
+  { id: 5, img: "src/components/assets/triangle.png", status: "back" },
+  { id: 5, img: "src/components/assets/triangle.png", status: "back" },
+  { id: 6, img: "src/components/assets/triangleRectangle.png", status: "back" },
+  { id: 6, img: "src/components/assets/triangleRectangle.png", status: "back" },
+  { id: 7, img: "src/components/assets/twoStars.png", status: "back" },
+  { id: 7, img: "src/components/assets/twoStars.png", status: "back" },
+  { id: 8, img: "src/components/assets/yellowStar.png", status: "back" },
+  { id: 8, img: "src/components/assets/yellowStar.png", status: "back" },
+].sort(() => Math.random() - 0.5);
 
-const cardsArrayDisplay = cardsArray.map((card, index) => {
-  return (
-    <div key={index} className="card">
-      <img src={card.img}/>
-    </div>
-  );
-});
+export default function Cards() {
+  const [cards, setCards] = useState(cardsArray);
+  const [prev, setPrev] = useState(-1);
 
-const Cards = () => {
-  const [card, setCard] = useState(cardsArray);
+  function handleClick(index) {
+    cardsArray[index].status = "active";
+    setCards([...cards]);
+  }
+
+  const cardsArrayDisplay = cardsArray.map((card, index) => {
+    return (
+      <div
+        key={index}
+        className={"card " + card.status}
+        onClick={() => handleClick(index)}
+      >
+        <img src={card.img} />
+      </div>
+    );
+  });
+
   return <div id="container">{cardsArrayDisplay}</div>;
-};
-
-export default Cards;
+}
