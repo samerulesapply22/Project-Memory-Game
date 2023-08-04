d080576990f267f23b5574fe99a6ef242dfb3b80
samerulesapply22 <curryballato@gmail.com>
2023-08-04 18:34:21 +0300

    Add 'new game' button

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,45 +1,43 @@
 import { useState } from "react";
 import Card from "./card";
 
-const cardsArray = [
-  { id: 1, img: "src/components/assets/happyFace.png", status: "back" },
-  { id: 2, img: "src/components/assets/happyFace.png", status: "back" },
-  { id: 3, img: "src/components/assets/horse.png", status: "back" },
-  { id: 4, img: "src/components/assets/horse.png", status: "back" },
-  { id: 5, img: "src/components/assets/redStar.png", status: "back" },
-  { id: 6, img: "src/components/assets/redStar.png", status: "back" },
-  { id: 7, img: "src/components/assets/treeStars.png", status: "back" },
-  { id: 8, img: "src/components/assets/treeStars.png", status: "back" },
-  { id: 9, img: "src/components/assets/triangle.png", status: "back" },
-  { id: 10, img: "src/components/assets/triangle.png", status: "back" },
-  {
-    id: 11,
-    img: "src/components/assets/triangleRectangle.png",
-    status: "back",
-  },
-  {
-    id: 12,
-    img: "src/components/assets/triangleRectangle.png",
-    status: "back",
-  },
-  { id: 13, img: "src/components/assets/twoStars.png", status: "back" },
-  { id: 14, img: "src/components/assets/twoStars.png", status: "back" },
-  { id: 15, img: "src/components/assets/yellowStar.png", status: "back" },
-  { id: 16, img: "src/components/assets/yellowStar.png", status: "back" },
-];
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-const shuffled = shuffle(cardsArray);
-
 export default function Cards() {
+  const cardsArray = [
+    { id: 1, img: "src/components/assets/happyFace.png", status: "back" },
+    { id: 2, img: "src/components/assets/happyFace.png", status: "back" },
+    { id: 3, img: "src/components/assets/horse.png", status: "back" },
+    { id: 4, img: "src/components/assets/horse.png", status: "back" },
+    { id: 5, img: "src/components/assets/redStar.png", status: "back" },
+    { id: 6, img: "src/components/assets/redStar.png", status: "back" },
+    { id: 7, img: "src/components/assets/treeStars.png", status: "back" },
+    { id: 8, img: "src/components/assets/treeStars.png", status: "back" },
+    { id: 9, img: "src/components/assets/triangle.png", status: "back" },
+    { id: 10, img: "src/components/assets/triangle.png", status: "back" },
+    {
+      id: 11,
+      img: "src/components/assets/triangleRectangle.png",
+      status: "back",
+    },
+    {
+      id: 12,
+      img: "src/components/assets/triangleRectangle.png",
+      status: "back",
+    },
+    { id: 13, img: "src/components/assets/twoStars.png", status: "back" },
+    { id: 14, img: "src/components/assets/twoStars.png", status: "back" },
+    { id: 15, img: "src/components/assets/yellowStar.png", status: "back" },
+    { id: 16, img: "src/components/assets/yellowStar.png", status: "back" },
+  ];
   const [prev, setPrev] = useState(false);
-  const [deck, setDeck] = useState(shuffled);
+  const [deck, setDeck] = useState(shuffle(cardsArray));
 
+  function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
   function newStatus(status, ...cards) {
     cards.forEach((card) => (card.status = status));
     setDeck([...deck]);
@@ -70,7 +68,7 @@ export default function Cards() {
     }
   }
 
-  return shuffled.map((card, index) => {
+  return deck.map((card, index) => {
     return (
       <Card key={index} card={card} handleClick={() => handleClick(card)} />
     );
--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,35 +1,22 @@
-import { useState } from "react";
 import Cards from "./cards";
 
 export default function Game() {
-  const [isGame, setIsGame] = useState("yes");
-  const game = () => {
-    setIsGame("yes");
-  };
-
-  if (isGame == "yes")
-    return (
-      <>
-        <h1>Jeu de mémoire</h1>
-        <div id="container"><Cards /></div>
-      </>
-    );
-  else if (isGame === "no")
-    return (
-      <div class="game">
-        <h1>Jeu de mémoire</h1>
-        <button id="newGame" onClick={game}>
-          Nouveau Jeu
+  return (
+    <>
+      <div id="header">
+        <h1>Memory game</h1>
+        <button
+          className="btnNewGame"
+          onClick={() => {
+            window.location.reload(false);
+          }}
+        >
+          New Game
         </button>
       </div>
-    );
-  else if (isGame === "over")
-    return (
-      <div class="game">
-        <h2>Fin du jeu. Vous gagnez!</h2>
-        <button id="newGame" onClick={game}>
-          Nouveau Jeu
-        </button>
+      <div id="container">
+        <Cards />
       </div>
-    );
+    </>
+  );
 }
